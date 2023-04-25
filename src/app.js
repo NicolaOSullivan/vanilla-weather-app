252a00a7eff5c24b350f618b0341c41e5b4b08ad
NicolaOSullivan <nicola.osullivan6@gmail.com>
2023-04-25 13:35:28 +0100

    Made details updateable based on location

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let query = `Dublin`;
 let apiKey = `cb5f738714fe5atbaff9a9f3oa9f3610`;
